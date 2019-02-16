b0dd871ea640715e312bf12ab6baf5f2088d1dc6
Pinkie Aruwani <aruwanip@gmail.com>
2019-02-16 13:34:36 -0500

    Full demo

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -31,13 +31,66 @@ module.exports = [
     "clobbers": [
       "CameraPopoverHandle"
     ]
+  },
+  {
+    "id": "cordova-call.CordovaCall",
+    "file": "plugins/cordova-call/www/CordovaCall.js",
+    "pluginId": "cordova-call",
+    "clobbers": [
+      "cordova.plugins.CordovaCall"
+    ]
+  },
+  {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open",
+      "window.open"
+    ]
+  },
+  {
+    "id": "cordova-plugin-call-number.CallNumber",
+    "file": "plugins/cordova-plugin-call-number/www/CallNumber.js",
+    "pluginId": "cordova-plugin-call-number",
+    "clobbers": [
+      "call"
+    ]
+  },
+  {
+    "id": "cordova-plugin-geolocation.geolocation",
+    "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "clobbers": [
+      "navigator.geolocation"
+    ]
+  },
+  {
+    "id": "cordova-plugin-geolocation.PositionError",
+    "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "runs": true
+  },
+  {
+    "id": "cordova-plugin-nativegeocoder.NativeGeocoder",
+    "file": "plugins/cordova-plugin-nativegeocoder/www/NativeGeocoder.js",
+    "pluginId": "cordova-plugin-nativegeocoder",
+    "clobbers": [
+      "nativegeocoder"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+  "cordova-plugin-camera": "4.0.3",
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-camera": "4.0.3"
+  "cordova-call": "1.1.6",
+  "cordova-plugin-inappbrowser": "3.0.0",
+  "cordova-plugin-call-number": "1.0.1",
+  "cordova-plugin-vibration": "3.1.0",
+  "cordova-plugin-geolocation": "4.0.1",
+  "cordova-plugin-nativegeocoder": "3.1.3"
 };
 // BOTTOM OF METADATA
 });
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,103 +1,66 @@
 <template>
   <v-layout row>
     <v-flex xs12 sm6 offset-sm3>
+      <v-img id="myImage" src="https://pearlhacks.com/img/logo.png" height="300px" style="background:#6DB3F2;"></v-img>
       <v-card>
-        <v-img
-          id="myImage"
-          src="https://cdn.vuetifyjs.com/images/lists/ali.png"
-          height="300px"
-        >
-          <v-layout
-            column
-            fill-height
-          >
-            <v-card-title>
-              <v-btn dark icon>
-                <v-icon>chevron_left</v-icon>
-              </v-btn>
-
-              <v-spacer></v-spacer>
-
-              <v-btn @click="takePicture()" dark icon class="mr-3">
-                <v-icon>edit</v-icon>
-              </v-btn>
-
-              <v-btn dark icon>
-                <v-icon>more_vert</v-icon>
-              </v-btn>
-            </v-card-title>
-
-            <v-spacer></v-spacer>
-
-            <v-card-title class="white--text pl-5 pt-5">
-              <div class="display-1 pl-5 pt-5">Ali Conners</div>
-            </v-card-title>
-          </v-layout>
-        </v-img>
+        <v-card-title class="pl-5 pb-0">
+          <div class="display-1 pl-5">Pearl Hacks</div>
+        </v-card-title>       
 
         <v-list two-line>
-          <v-list-tile>
+          <v-list-tile @click="makeCall()">
             <v-list-tile-action>
               <v-icon color="indigo">phone</v-icon>
             </v-list-tile-action>
-
             <v-list-tile-content>
-              <v-list-tile-title>(650) 555-1234</v-list-tile-title>
-              <v-list-tile-sub-title>Mobile</v-list-tile-sub-title>
-            </v-list-tile-content>
-
-            <v-list-tile-action>
-              <v-icon>chat</v-icon>
-            </v-list-tile-action>
-          </v-list-tile>
-
-          <v-list-tile>
-            <v-list-tile-action></v-list-tile-action>
-
-            <v-list-tile-content>
-              <v-list-tile-title>(323) 555-6789</v-list-tile-title>
-              <v-list-tile-sub-title>Work</v-list-tile-sub-title>
-            </v-list-tile-content>
-
-            <v-list-tile-action>
-              <v-icon>chat</v-icon>
-            </v-list-tile-action>
-          </v-list-tile>
-
-          <v-divider inset></v-divider>
-
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon color="indigo">mail</v-icon>
-            </v-list-tile-action>
-
-            <v-list-tile-content>
-              <v-list-tile-title>aliconnors@example.com</v-list-tile-title>
-              <v-list-tile-sub-title>Personal</v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-
-          <v-list-tile>
-            <v-list-tile-action></v-list-tile-action>
-
-            <v-list-tile-content>
-              <v-list-tile-title>ali_connors@example.com</v-list-tile-title>
-              <v-list-tile-sub-title>Work</v-list-tile-sub-title>
+              <v-list-tile-title>{{phoneNumberString}}</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
 
           <v-divider inset></v-divider>
 
-          <v-list-tile>
+          <v-list-tile @click="openBrowser(website)">
+            <v-list-tile-action>
+              <v-icon color="indigo">open_in_browser</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{website}}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-divider inset></v-divider>
+
+          <v-list-tile @click="vibratePhone()">
+            <v-list-tile-action>
+              <v-icon color="indigo">notifications_active</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Vibrate phone</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-divider inset></v-divider>
+
+          <v-list-tile @click="getLocation()">
             <v-list-tile-action>
               <v-icon color="indigo">location_on</v-icon>
             </v-list-tile-action>
-
             <v-list-tile-content>
-              <v-list-tile-title>1400 Main Street</v-list-tile-title>
-              <v-list-tile-sub-title>Orlando, FL 79938</v-list-tile-sub-title>
+              <v-list-tile-title>Get Location</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
+
+          <v-divider inset></v-divider>
+
+          <v-list-tile @click="openCamera()">
+            <v-list-tile-action>
+              <v-icon color="indigo">camera_alt</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Open camera</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
         </v-list>
       </v-card>
     </v-flex>
@@ -108,22 +71,83 @@
   export default {
     data() {
       return {
-
+        phoneNumber: 9194187940,
+        phoneNumberString: '(919) 481-7940',
+        website: 'https://www.instagram.com/pearlhacks/'
+           
       }
     },
     methods: {
-      cameraError(error) {
-        console.error(error);
+      callError(message) {
+        alert('There was an error making the call. Error: ', message);
+      },
+      callSuccess(result) {
+        // do nothing
+      },
+      cameraError(message) {
+        alert('There was a problem taking a picture. Error: ', message);
       },
       cameraSuccess(imageData) {
-        document.getElementById('myImage').setAttribute('src', 'data:image/jpeg;base64,' + imageData);
+        // do nothing
       },
-      takePicture() {
-        console.log('navigator:', navigator);
-        navigator.camera.getPicture(this.cameraSuccess, this.cameraError, {  
-          quality: 50,
-          destinationType: Camera.DestinationType.DATA_URL 
-        });
+      getLocation() {
+        let options = {
+          enableHighAccuracy: true,
+          maximumAge: '3600000'
+        }
+        let watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+        let latitude = '';
+        let longitude = '';
+        function onSuccess(position) {
+          latitude = position.coords.latitude;
+          longitude = position.coords.longitude;
+          nativegeocoder.reverseGeocode(success, failure, latitude, longitude, { useLocale: true, maxResults: 1 });
+         };
+      
+         function onError(error) {
+            alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+         }
+ 
+         function success(result) {
+            //$scope.message = "getting address in fu";
+         let firstResult = result[0];
+         let address = JSON.stringify(firstResult);
+         let position = JSON.parse(address);
+        // this.postalCode = position.postalCode;
+            alert(  position.subThoroughfare + ' ' + position.thoroughfare + '\n' +
+        position.locality          + '\n' +
+        position.countryName      + ','  +  position.postalCode          + '\n' );
+        }
+         
+         function failure(err) {
+         console.log(err);
+         }
+      },
+      loadErrorCallback(error) {
+        alert('Error loading page: ', error.message);
+      },
+      makeCall() {
+        window.cordova.plugins.CordovaCall.callNumber(this.phoneNumber, this.callSuccess, this.callError);
+      },
+      openBrowser(url) {
+        let target = '_blank';
+        let options = 'location = yes';
+        let ref = cordova.InAppBrowser.open(url, target, options);
+        ref.addEventListener('loaderror', loaderrorCallback);
+      },
+      openCamera() {
+        navigator.camera.getPicture(this.cameraSuccess, this.cameraError, 
+          {
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            mediaType: Camera.MediaType.PICTURE
+          }
+        );
+      },
+      vibratePhone() {
+        let time = 1000;
+        navigator.vibrate(time);
+        alert('Deutsche Bank wishes you good luck!');
       }
     }
   }
