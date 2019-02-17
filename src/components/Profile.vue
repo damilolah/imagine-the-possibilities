7af6d2fa4eeb712a5e689f249473a28ff76cc837
Pinkie Aruwani <aruwanip@gmail.com>
2019-02-16 20:26:52 -0500

    updates

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,13 +1,19 @@
 <template>
   <v-layout row>
     <v-flex xs12 sm6 offset-sm3>
-      <v-img id="myImage" src="https://pearlhacks.com/img/logo.png" height="300px" style="background:#6DB3F2;"></v-img>
       <v-card>
-        <v-card-title class="pl-5 pb-0">
-          <div class="display-1 pl-5">Pearl Hacks</div>
-        </v-card-title>       
+
+        <v-img src="https://www.cntry.biz/wp-content/uploads/2015/12/Female-Coders.jpg" height="300px">
+          <v-layout column fill-height>
+            <v-spacer></v-spacer>
+            <v-card-title class="white--text pl-5 pt-5">
+              <div class="display-1 pl-5 pt-5">Pearl Hacks</div>
+            </v-card-title>
+          </v-layout>
+        </v-img>
 
         <v-list two-line>
+
           <v-list-tile @click="makeCall()">
             <v-list-tile-action>
               <v-icon color="indigo">phone</v-icon>
@@ -46,7 +52,7 @@
               <v-icon color="indigo">location_on</v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
-              <v-list-tile-title>Get Location</v-list-tile-title>
+              <v-list-tile-title>Get location</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
 
@@ -68,80 +74,87 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        phoneNumber: 9194187940,
-        phoneNumberString: '(919) 481-7940',
-        website: 'https://www.instagram.com/pearlhacks/'
-           
+export default {
+  data () {
+    return {
+      phoneNumber: 9194187940,
+      phoneNumberString: '(919) 481-7940',
+      website: 'https://www.instagram.com/pearlhacks/'
+    }
+  },
+  methods: {
+    getLocation () {
+      let options = {
+        enableHighAccuracy: true,
+        maximumAge: '3600000'
+      }
+      navigator.geolocation.getCurrentPosition(onSuccess, onError, options)
+      let latitude = ''
+      let longitude = ''
+      function onSuccess (location) {
+        latitude = location.coords.latitude
+        longitude = location.coords.longitude
+        if (window.cordova.platformId === 'browser') {
+          // cordova-plugin-nativegeocoder does not work on browser, display latitude and longitude
+          alert('Your location:\nLatitidue: ' + latitude + '\nLongitude: ' + longitude)
+        } else {
+          // Reverse geolocation, display address location
+          window.nativegeocoder.reverseGeocode(success, failure, latitude, longitude, {useLocale: true, maxResults: 1})
+        }
+      }
+      function onError (error) {
+        alert('There was a problem getting your geolocation.\nError: ' + error.message)
+      }
+      function success (result) {
+        let firstResult = result[0]
+        let address = JSON.stringify(firstResult)
+        let position = JSON.parse(address)
+        alert(position.subThoroughfare + ' ' + position.thoroughfare + '\n' + position.locality + ', ' +
+            position.administrativeArea + ' ' + position.postalCode + '\n' + position.countryCode)
+      }
+      function failure (error) {
+        alert('There was an error translating your location. Error: ' + error)
       }
     },
-    methods: {
-      getLocation() {
-        let options = {
-          enableHighAccuracy: true,
-          maximumAge: '3600000'
-        }
-        let watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
-        let latitude = '';
-        let longitude = '';
-        function onSuccess(position) {
-          latitude = position.coords.latitude;
-          longitude = position.coords.longitude;
-          // Translate latitude and longitude
-          nativegeocoder.reverseGeocode(success, failure, latitude, longitude, {useLocale: true, maxResults: 1});
-        }
-        function onError(error) {
-          alert('code: ' + error.code + '\nmessage: ' + error.message);
-        }
-        function success(result) {
-          let address = JSON.stringify(result[0]);
-          let position = JSON.parse(address);
-          alert(position + '\n' + position.subThoroughfare + ' ' + position.thoroughfare + '\n' + position.locality + '\n' +
-              position.countryName  + ', '  +  position.postalCode+ '\n');
-        }
-        function failure(error) {
-          alert('There was an error translating your location. Error: ', error);
-        }
-      },
-      makeCall() {
-        function callError(message) {
-          alert('There was an error making the call. Error: ', message);
-        }
-        function callSuccess(result) {
-          // do nothing
-        }
-        window.cordova.plugins.CordovaCall.callNumber(this.phoneNumber, this.callSuccess, this.callError);
-      },
-      openBrowser(url) {
-        let target = '_blank';
-        let options = 'location = yes';
-        let ref = cordova.InAppBrowser.open(url, target, options);
-        function loadErrorCallback(error) {
-          alert('Error loading page: ', error.message);
-        }
-        ref.addEventListener('loaderror', loaderrorCallback);
-      },
-      openCamera() {
-        function cameraError(error) {
-          alert('There was a problem taking a picture. Error: ', error);
-        }
-        function cameraSuccess(result) {
-          // do nothing
-        }
-        let options = {
-          destinationType: Camera.DestinationType.FILE_URI,
-          sourceType: Camera.PictureSourceType.CAMERA,
-          mediaType: Camera.MediaType.PICTURE
-        }
-        navigator.camera.getPicture(cameraSuccess, cameraError, options);
-      },
-      vibratePhone() {
-        let time = 1000;
-        navigator.vibrate(time);
-        alert('Deutsche Bank wishes you good luck!');
+    makeCall () {
+      function callError (message) {
+        alert('There was an error making the call. Error: ' + message)
       }
+      function callSuccess (result) {
+        // do nothing
+      }
+      window.cordova.plugins.CordovaCall.callNumber(this.phoneNumber, callSuccess, callError)
+    },
+    openBrowser (url) {
+      let cordova
+      let target = '_blank'
+      let options = 'location = yes'
+      function loadErrorCallback (error) {
+        alert('Error loading page: ' + error.message)
+      }
+      let ref = cordova.InAppBrowser.open(url, target, options)
+      ref.addEventListener('loaderror', loadErrorCallback)
+    },
+    openCamera () {
+      function cameraError (error) {
+        alert('There was a problem taking a picture. Error: ' + error)
+      }
+      function cameraSuccess (result) {
+        // do nothing
+      }
+      let Camera
+      let options = {
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        mediaType: Camera.MediaType.PICTURE
+      }
+      navigator.camera.getPicture(cameraSuccess, cameraError, options)
+    },
+    vibratePhone () {
+      let time = 1000
+      navigator.vibrate(time)
+      alert('Deutsche Bank wishes you good luck!')
     }
   }
+}
 </script>
