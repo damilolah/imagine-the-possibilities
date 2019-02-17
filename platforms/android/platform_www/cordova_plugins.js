58225ae53d3b17c7ed06b4412842b99fc72d85cb
Pinkie Aruwani <aruwanip@gmail.com>
2019-02-16 20:41:10 -0500

    update

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,6 +1,22 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
+    "id": "cordova-call.CordovaCall",
+    "file": "plugins/cordova-call/www/CordovaCall.js",
+    "pluginId": "cordova-call",
+    "clobbers": [
+      "cordova.plugins.CordovaCall"
+    ]
+  },
+  {
+    "id": "cordova-plugin-call-number.CallNumber",
+    "file": "plugins/cordova-plugin-call-number/www/CallNumber.js",
+    "pluginId": "cordova-plugin-call-number",
+    "clobbers": [
+      "call"
+    ]
+  },
+  {
     "id": "cordova-plugin-camera.Camera",
     "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
     "pluginId": "cordova-plugin-camera",
@@ -33,31 +49,6 @@ module.exports = [
     ]
   },
   {
-    "id": "cordova-call.CordovaCall",
-    "file": "plugins/cordova-call/www/CordovaCall.js",
-    "pluginId": "cordova-call",
-    "clobbers": [
-      "cordova.plugins.CordovaCall"
-    ]
-  },
-  {
-    "id": "cordova-plugin-inappbrowser.inappbrowser",
-    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
-    "pluginId": "cordova-plugin-inappbrowser",
-    "clobbers": [
-      "cordova.InAppBrowser.open",
-      "window.open"
-    ]
-  },
-  {
-    "id": "cordova-plugin-call-number.CallNumber",
-    "file": "plugins/cordova-plugin-call-number/www/CallNumber.js",
-    "pluginId": "cordova-plugin-call-number",
-    "clobbers": [
-      "call"
-    ]
-  },
-  {
     "id": "cordova-plugin-geolocation.geolocation",
     "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
     "pluginId": "cordova-plugin-geolocation",
@@ -72,6 +63,15 @@ module.exports = [
     "runs": true
   },
   {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open",
+      "window.open"
+    ]
+  },
+  {
     "id": "cordova-plugin-nativegeocoder.NativeGeocoder",
     "file": "plugins/cordova-plugin-nativegeocoder/www/NativeGeocoder.js",
     "pluginId": "cordova-plugin-nativegeocoder",
@@ -83,14 +83,14 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-camera": "4.0.3",
-  "cordova-plugin-whitelist": "1.3.3",
   "cordova-call": "1.1.6",
-  "cordova-plugin-inappbrowser": "3.0.0",
   "cordova-plugin-call-number": "1.0.1",
-  "cordova-plugin-vibration": "3.1.0",
+  "cordova-plugin-camera": "4.0.3",
   "cordova-plugin-geolocation": "4.0.1",
-  "cordova-plugin-nativegeocoder": "3.1.3"
+  "cordova-plugin-inappbrowser": "3.0.0",
+  "cordova-plugin-nativegeocoder": "3.1.3",
+  "cordova-plugin-vibration": "3.1.0",
+  "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
